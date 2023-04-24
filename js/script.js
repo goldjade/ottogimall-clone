c257a3bec293c5ced3ad73124afd875b44a4ff9c
choi <bcaca0831@gmail.com>
2023-04-25 00:40:07 +0900

    swiper / css 적용예정

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-window.onload = function () {};
+window.onload = function () {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
