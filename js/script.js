2d85b2ec5286dd545b3f48d9025b669963d59a50
choi <bcaca0831@gmail.com>
2023-05-07 23:20:15 +0900

    헤더 펼침메뉴 작업중

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 window.onload = function () {
-  var swiper = new Swiper(".mySwiper", {
+  const topBanner = document.querySelector(".top-banner");
+  const topBannerCloseBtn = document.querySelector(".top-banner-close");
+  const wrap = document.querySelector(".wrap");
+  const header = document.querySelector(".header");
+
+  topBannerCloseBtn.addEventListener("click", function () {
+    topBanner.style.display = "none";
+  });
+
+  window.addEventListener("scroll", function () {
+    let scrY = this.scrollY;
+  });
+
+  let swVisual = new Swiper(".sw-visual", {
     loop: true,
     autoplay: {
-      delay: 1000,
+      delay: 2000,
       disableOnInteraction: false,
     },
+
     pagination: {
-      el: ".swiper-pagination",
+      el: ".visual-pg",
       type: "fraction",
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  });
+
+  //슬라이드 멈춤 기능
+  const swVisualPlay = document.querySelector(".visual-play");
+  swVisualPlay.addEventListener("click", function () {
+    if (swVisualPlay.classList.contains("active")) {
+      swVisual.autoplay.start();
+      swVisualPlay.classList.remove("active");
+    } else {
+      swVisual.autoplay.stop();
+      swVisualPlay.classList.add("active");
+    }
   });
 };
