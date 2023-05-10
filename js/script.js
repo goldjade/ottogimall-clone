6cff19dfa49c996edf4835824ce45875ccc8321a
choi <bcaca0831@gmail.com>
2023-05-10 11:39:07 +0900

    전체 카테고리 완료

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
 window.onload = function () {
   const topBanner = document.querySelector(".top-banner");
   const topBannerCloseBtn = document.querySelector(".top-banner-close");
-  const wrap = document.querySelector(".wrap");
-  const header = document.querySelector(".header");
-
-  topBannerCloseBtn.addEventListener("click", function () {
-    topBanner.style.display = "none";
-  });
 
   window.addEventListener("scroll", function () {
     let scrY = this.scrollY;
   });
 
+  topBannerCloseBtn.addEventListener("click", function () {
+    topBanner.style.display = "none";
+  });
+
+  const cateBtn = document.querySelector(".cate");
+  const cateList = document.querySelector(".cate-list");
+  // 전체메뉴
+  cateBtn.addEventListener("mouseover", function () {
+    cateList.classList.add("active");
+  });
+  cateBtn.addEventListener("mouseleave", function () {
+    cateList.classList.remove("active");
+  });
+  // 서브메뉴
+  const cateLists = document.querySelectorAll(".mainCategory");
+  const cateDepth2 = document.querySelectorAll(".subCategory");
+  cateLists.forEach(function (item, index) {
+    item.addEventListener("mouseenter", function () {
+      cateDepth2.forEach(function (itemSub, indexSub) {
+        itemSub.style.display = "none";
+        if (indexSub === index) {
+          itemSub.style.display = "block";
+        }
+        if (itemSub.children.length === 0) {
+          itemSub.style.display = "none";
+        }
+      });
+    });
+  });
+
+  // 비주얼 슬라이드
   let swVisual = new Swiper(".sw-visual", {
     loop: true,
     autoplay: {
